9c04a18ff9c8b5152a6a7ac22269f04273e31f6f
acanest83 <133660942+acanest83@users.noreply.github.com>
2023-10-26 21:20:21 +0200

    Falta estilos

--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,21 @@
-// Your code here
+const FoodBox = ({ food, onDelete }) => {
+    const { id,name, calories, image, servings } = food;
+    const totalCalories = servings * calories;
+  const handleDelete = () => {
+    onDelete(id);
+  };
+    return (
+      <div className="food-box">
+        <p>{name}</p>
+        <img src={image} alt={name} />
+        <p>Calories: {calories}</p>
+        <p>Servings: {servings}</p>
+        <p>
+          <b>Total Calories: {totalCalories} kcal</b>
+        </p>
+        <button onClick={handleDelete}>Delete</button>
+      </div>
+    );
+  };
+  
+  export default FoodBox;
